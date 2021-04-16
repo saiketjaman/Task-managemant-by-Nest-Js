072f87087c71eece8d163067ff908a2baa4a3450
saikatjaman <saikatjaman@gmail.com>
2021-04-16 18:32:59 +0800

    add code_base environment

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -6,6 +6,8 @@ import { Strategy, ExtractJwt } from "passport-jwt";
 import { JwtPayload } from "./jwt-payload.interface";
 import { User } from "./user.entity";
 import { UserRepository } from "./user.repository";
+import * as config from 'config';
+const jwtConfig = config.get('jwt');
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -15,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'topSecret51',
+            secretOrKey: process.env.JWT_SECRET || jwtConfig.secret,
         });
     }
     
